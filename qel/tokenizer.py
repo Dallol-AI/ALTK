1d18b54ce142982d66d323404666565ef6630215
Abdulmunim <amunim729@gmail.com>
2023-12-19 09:54:05 +0300

    Added compound word handler

--- qel/tokenizer.py
+++ qel/tokenizer.py
@@ -2,7 +2,7 @@ import re, string
 from typing import List
 from utils import apply_rules, RULES
 
-# TODO: HANDLE COMPOUND WORDS SUCH AS ስነz
+# TODO: HANDLE COMPOUND WORDS SUCH AS ስነ
 
 class AmharicTokenizer:
     def __init__(self, word_delimiters: set = None, sentence_delimiters: set = None):
@@ -13,9 +13,9 @@ class AmharicTokenizer:
         - sentence_punctuations (Set[str]): List of sentence-ending punctuations.
         - word_punctuations (Set[str]): List of word-ending punctuations.
         """
-        self.word_delimiters = word_delimiters or { "[", " ", "፣", "።", ",", "፦", "!", ">", "&", "፧", "}", "^", ")", "፨", "<", "~", "]", "*", "{", "፤", "/", "፥", "(", "\\", "_", "+", ";", "#", "\"", ":", "=", " ", "%", "|", "`", "@", "'", "?", "$", }
-        self.sentence_delimiters = sentence_delimiters or ["።", "፥", "፨", "::", "፡፡", "?", "!",'፧']
-        self.compound_words_fix = [
+        self.__word_delimiters = word_delimiters or { "[", " ", "፣", "።", ",", "፦", "!", ">", "&", "፧", "}", "^", ")", "፨", "<", "~", "]", "*", "{", "፤", "/", "፥", "(", "\\", "_", "+", ";", "#", "\"", ":", "=", " ", "%", "|", "`", "@", "'", "?", "$", }
+        self.__sentence_delimiters = sentence_delimiters or ["።", "፥", "፨", "::", "፡፡", "?", "!",'፧']
+        self.__compound_words_fix = [
             'ስነ','ቤተ', 'እግረ','ሥነ'
         ]
 
@@ -30,8 +30,8 @@ class AmharicTokenizer:
         """
         if clean: text = apply_rules(text, RULES)
         
-        delimiters = self.word_delimiters
-        compound_words = seld.compound_words_fix
+        delimiters = self.__word_delimiters
+        compound_words = self.__compound_words_fix
         
         tokens = []
         curr_word = ""
@@ -43,7 +43,7 @@ class AmharicTokenizer:
             else:
                 curr_word = curr_word.strip()
                 if curr_word:
-                    if compound_words_as_one and curr_word:
+                    if compound_words_as_one and curr_word in self.__compound_words_fix:
                         continue
                     tokens.append(curr_word)
                     prev_word = curr_word
@@ -55,7 +55,7 @@ class AmharicTokenizer:
             tokens.append(curr_word.strip())
         return tokens
 
-    def sentence_tokenize(self, text: str, clean=False):
+    def sentence_tokenize(self, text: str, clean=True):
         """
         Tokenize a text into a list of sentences.
 
@@ -63,16 +63,13 @@ class AmharicTokenizer:
         :param clean: apply basic cleaning rules to the text
         :return: a list of sentences
         """
-        # preprocess the text
-        text = re.sub("\s+", " ", text) # replace multiple space with one
-        text = re.sub("\n", "።", text) # replace newlines with sentence delimiter
         if clean: text = apply_rules(text, RULES)
                 
         sentences = []
         current_sentence = ""
     
         for char in sentences:
-            if char not in self.sentence_delimiters:
+            if char not in self.__sentence_delimiters:
                 current_sentence += char
             elif current_sentence.strip():
                 sentences.append(current_sentence.strip())
@@ -82,10 +79,10 @@ class AmharicTokenizer:
             sentences.append(current_sentence.strip())
         return sentences
 
+
     @classmethod
     def __find_indexes(text, punct):
         """
         returns the index of the sentence delimiters in the text
         """
         return [i + len(punct) - 1 for i in range(len(text)) if text.startswith(punct, i)]
-        
